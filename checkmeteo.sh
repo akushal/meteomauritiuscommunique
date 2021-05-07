66082dc7222b13996c9f9bd306bc8a454ae136a8
Kushal appadu <appadudevraj7@hotmail.com>
2021-05-07 07:46:40 +0400

    Add emoji on warning and  green communiquer

--- checkmeteo.sh
+++ checkmeteo.sh
@@ -2,4 +2,16 @@
 
 URL=http://metservice.intnet.mu/warning-bulletin-special-weather.php
 
-curl -Ls $URL | grep -oP '<span style="color: #ff0000">[^*]+' |sed -e 's/<[^>]*>//g'
+result=`curl -Ls $URL | grep -oP '<span style="color: #ff0000"[^*]+' |sed -e 's/<[^>]*>//g'`
+
+if [ -z $result ]; then
+	result=`curl -Ls $URL | grep -oP '<span style="color: #4A7FBB"[^*]+' |sed -e 's/<[^>]*>//g'`
+	if [ ! -z "$result" ];then
+		result="✅$result"
+	fi
+else
+   result="⚠️ $result"
+fi
+
+echo $result
+
