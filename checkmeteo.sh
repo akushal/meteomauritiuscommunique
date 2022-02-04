7a61cecbe0a85092d49f9a561d4165e6f008e476
Kushal appadu <appadudevraj7@hotmail.com>
2022-02-04 21:33:03 +0400

    Update checkmeteo script with new change on meteo website

--- checkmeteo.sh
+++ checkmeteo.sh
@@ -2,9 +2,19 @@
 
 URL=http://metservice.intnet.mu/warning-bulletin-special-weather.php
 
-result=`curl -Ls $URL | grep -oP '<span style="color: #ff0000"[^*]+' |sed -e 's/<[^>]*>//g'`
 
-if [ -z $result ]; then
+#Warning Communique pale blue
+result=`curl -Ls $URL | grep -oP '<span style="color:#4A7FBB"[^*]+' |sed -e 's/<[^>]*>//g'` 
+
+if [ -z "$result" ]; then
+	#Special Communique red
+	result=`curl -Ls $URL |egrep -i '#ff0000"[^*]+' |sed -e 's/<[^>]*>//g' |  sed -e 's/^[[:space:]]*//'`
+	#result=`curl -Ls $URL | grep -oP '<span style="color: #ff0000"[^*]+' |sed -e 's/<[^>]*>//g'`
+fi
+
+
+if [ -z "$result" ]; then
+	#All the warning is over blue
 	result=`curl -Ls $URL | grep -oP '<span style="color: #4A7FBB"[^*]+' |sed -e 's/<[^>]*>//g'`
 	if [ ! -z "$result" ];then
 		result="✅$result"
@@ -13,8 +23,8 @@ else
    result="⚠️ $result"
 fi
 
-if [ ! -z $result ]; then
-	echo $result
+if [ ! -z "$result" ]; then
+	echo $result | sed -e "s/'//g"
 else
 	echo "✅ No warning in Mauritius"
 fi
